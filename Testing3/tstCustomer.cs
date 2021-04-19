d77e38e6c2426b107c9262659cf1f6e63a8e90d7
P2521336 <P2521336@KLIXX104WIN002W>
2021-04-19 13:45:15 +0100

    Created a Class and test.

---
+++ Testing3/tstCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstantOK()
+        {
+            //create an instance of the class I want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
